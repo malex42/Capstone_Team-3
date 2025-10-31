c206a9cb7baac4d983930fd88eb19269084670fc
Elizabeth Myatt <104538620+ElizabethMyatt@users.noreply.github.com>
2025-10-30 20:39:27 -0500

    Adding libraries for testing the route

--- server/tests/test_routes.py
+++ server/tests/test_routes.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
-@app.route('/api/auth/register', methods=['POST'])
+import pytest
+import json
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from unittest.mock import Mock, MagicMock, patch
+from datetime import datetime, timedelta
+from werkzeug.routing import ValidationError
 
