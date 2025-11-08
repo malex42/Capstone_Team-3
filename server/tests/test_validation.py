0a9240fc292931c2bdf450598ff9727a5b07ad02
Elizabeth Myatt <104538620+ElizabethMyatt@users.noreply.github.com>
2025-11-07 20:41:14 -0600

    Add user inputs for testing validation of user input

--- server/tests/test_validation.py
+++ server/tests/test_validation.py
@@ -1,3 +1,10 @@
 import pytest
 from handlers.validation_handler import ValidationHandler
 
+class TestValidation:
+    def test_validation_user_inputs(self):
+
+        user_inputs = [
+                "",
+                ""
+        ]
