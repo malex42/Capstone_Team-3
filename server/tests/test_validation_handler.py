cd1fcdd92b6a83e2489285c063f320b3d30f9f28
Elizabeth Myatt <104538620+ElizabethMyatt@users.noreply.github.com>
2025-11-22 16:31:33 -0600

    Test that input validation works

--- server/tests/test_validation_handler.py
+++ server/tests/test_validation_handler.py
@@ -4,4 +4,12 @@ from handlers.validation_handler import ValidationHandler
 
 class TestValidationHandler:
     """Made unit tests for ValidationHandler"""
-    
+
+    def test_validate_valid_inputs(self):
+        """Test that input validation works"""
+        valid_inputs = [
+            "test@example",
+            "username"
+        ]
+        for input_text in valid_inputs:
+            assert ValidationHandler.validate_user_input(input_text) is True
