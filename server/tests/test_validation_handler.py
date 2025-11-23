a7d1340484d7816664115b2493e9cc6bcbca6efa
Elizabeth Myatt <104538620+ElizabethMyatt@users.noreply.github.com>
2025-11-22 19:48:23 -0600

    Test that xss attempts are allowed

--- server/tests/test_validation_handler.py
+++ server/tests/test_validation_handler.py
@@ -42,3 +42,14 @@ class TestValidationHandler:
         ]
         for malicious in malicious_inputs:
             assert ValidationHandler.validate_user_input(malicious) is False
+
+    def test_validate_input_xss_attempt(self):
+        """Test that xss attempt are blocked"""
+        xss_inputs = [
+            "<iframe src='malicious'>",
+            "<script>alert('XSS')</script>",
+            "javascript:alert(1)",
+            "<img src=x onerror=alert(1)>"
+        ]
+        for xss in xss_inputs:
+            assert ValidationHandler.validate_user_input(xss) is False
