0ca77d95d14b5af4b51aaafa55ec761593b9756f
Elizabeth Myatt <104538620+ElizabethMyatt@users.noreply.github.com>
2025-12-05 15:29:56 -0600

    Created test client, login endpoint and login success endpoint

--- server/tests/tests_api_endpoints.py
+++ server/tests/tests_api_endpoints.py
@@ -16,26 +16,37 @@ class MockDatabaseHandler:
         self.client = MongoClient()
         self.database = self.client['test_db']
 
-    @pytest.fixture
-    def app():
-        """Flask application fixture"""
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        app.config['JWT_SECRET_KEY'] = 'test-secret-key'
-        JWTManager(app)
+@pytest.fixture
+def app():
+    """Flask application fixture"""
+    app = Flask(__name__)
+    app.config['TESTING'] = True
+    app.config['JWT_SECRET_KEY'] = 'test-secret-key'
+    JWTManager(app)
 
-        #Create handlers
-        db_handler = MockDatabaseHandler()
-        pw_handler = PasswordHandler()
-        account_handler = AccountHandler(db_handler, pw_handler)
-        business_handler = BusinessHandler(db_handler)
-        schedule_handler = ScheduleHandler(db_handler)
+    #Create handlers
+    db_handler = MockDatabaseHandler()
+    pw_handler = PasswordHandler()
+    account_handler = AccountHandler(db_handler, pw_handler)
+    business_handler = BusinessHandler(db_handler)
+    schedule_handler = ScheduleHandler(db_handler)
 
-        # Setting up routes
-        from routes.routes import setup_routes
-        setup_routes(app, account_handler, business_handler, schedule_handler)
+    # Setting up routes
+    from routes.routes import setup_routes
+    setup_routes(app, account_handler, business_handler, schedule_handler)
 
-        # Clean database
-        account_handler.users_collection.delete_many({})
+    # Clean database
+    account_handler.users_collection.delete_many({})
 
-        return app
+    return app
+
+@pytest.fixture
+def client(app):
+    """Create test client"""
+    return app.test_client()
+
+class TestLoginEndpoint:
+    """Tests for Login Endpoint"""
+    def test_login_success(self, client):
+        """Test login success endpoint"""
+        client.post('/api/auth/register', json={'username': 'test_user', 'password': 'Password123', 'role':'employee'})
