b303759ded5afffd891f4f90a4f376afb16e5028
Elizabeth Myatt <104538620+ElizabethMyatt@users.noreply.github.com>
2025-12-04 21:53:03 -0600

    Having the flask application fixture

--- server/tests/tests_api_endpoints.py
+++ server/tests/tests_api_endpoints.py
@@ -17,3 +17,24 @@ class MockDatabaseHandler:
 
     @pytest.fixture
     def app():
+        """Flask application fixture"""
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        app.config['JWT_SECRET_KEY'] = 'test-secret-key'
+        JWTManager(app)
+
+        #Create handlers
+        db_handler = MockDatabaseHandler()
+        pw_handler = PasswordHandler()
+        account_handler = AccountHandler(db_handler, pw_handler)
+        business_handler = BusinessHandler(db_handler)
+        schedule_handler = ScheduleHandler(db_handler)
+
+        # Setting up routes
+        from routes.routes import setup_routes
+        setup_routes(app, account_handler, business_handler, schedule_handler)
+
+        # Clean database
+        account_handler.users_collection.delete_many({})
+
+        return app
